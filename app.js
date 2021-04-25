59a4582478f839b6908d6ba3383c2de289a05e66
leechannie <61904561+leechannie@users.noreply.github.com>
2021-04-25 12:43:36 +0900

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const msg = require('dialog');
 
 
 const app = express();
-const port = process.env.PORT;
+const port =process.env.PORT||8000;
 //app.set('port', process.env.PORT||8000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
